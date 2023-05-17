dfe9aabb068f60bc62c3a8246e1f9370d3d6bf09
CalrethonOfMirkwood <82109882+CalrethonOfMirkwood@users.noreply.github.com>
2023-05-17 11:34:40 -0700

    quickGrade

---
+++ quickGrade.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+$ echo Running quickGrade!
+
+# for each test case in data,
+# 1. Save input of test case as a bash variable
+
+# then run it for every file in studentCode:
+# 2. Run the code with input
+# 3. Save the output of student code as a bash variable
+# 4. Compare actual output to expected output
+# 5. Update XML table
