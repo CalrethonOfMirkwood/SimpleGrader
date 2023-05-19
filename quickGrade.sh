8024f3c1854c2087aea68368853a376094cdde11
CalrethonOfMirkwood <82109882+CalrethonOfMirkwood@users.noreply.github.com>
2023-05-19 10:50:42 -0700

    wow

--- quickGrade.sh
+++ quickGrade.sh
@@ -5,6 +5,18 @@ $ echo Running quickGrade\!
 # for each test case in data,
 # 1. Save input of test case as a bash variable
 
+data=()
+dir=./data
+for f in "$dir"/*.in; do
+    data+=("$f")
+done
+
+code=()
+dir=./studentCode
+for f in "$dir"/*; do
+    code+=("$f")
+done
+
 # then run it for every file in studentCode:
 # 2. Run the code with input
 # 3. Save the output of student code as a bash variable
